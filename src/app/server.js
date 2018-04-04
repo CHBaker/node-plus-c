fe019fe92d88792fbf15b932bbd6995035217ce3
CHBaker <charles.baker@besl.org>
2018-04-04 16:22:31 -0600

    STYLE: display c code with fresh looks

--- src/app/server.js
+++ src/app/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.set('view engine', 'hbs');
 const hbs = require('hbs');
+
 // import addon (c code)
 const addon = require('../../build/Release/obj.target/addon.node');
 
