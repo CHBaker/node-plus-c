f63c103f81f3d283c25c9cb01ef4b9b9e9bc122f
CHBaker <charles.baker@besl.org>
2018-04-04 15:09:35 -0600

    FEATURE: node executes addon c code  - gyp config - prints hello world - in build folder

--- src/app/server.js
+++ src/app/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+app.set('view engine', 'hbs');
+const hbs = require('hbs');
+// import addon (c code)
+const addon = require('../../build/Release/obj.target/addon.node');
 
 const port = 5050;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    const result = { 'result': addon.hello()}
+    res.render('index.hbs', result);
 });
 
 app.listen(port, () => {
